119f2fea2c2e5c44e22f743ff5473c47e85de8d3
oded282 <oded.bennoonshamir@live.biu.ac.il>
2018-12-17 15:33:41 +0200

    17.12 1533

---
+++ openDataServer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by oded on 12/17/18.
+//
+
+#include "openDataServer.h"
---
+++ openDataServer.h
@@ -0,0 +1,17 @@
+//
+// Created by oded on 12/17/18.
+//
+
+#ifndef FLIGHT_SIMULATOR_OPENDATASERVER_H
+#define FLIGHT_SIMULATOR_OPENDATASERVER_H
+
+
+#include "Expression.h"
+
+class openDataServer : public Expression {
+
+
+};
+
+
+#endif //FLIGHT_SIMULATOR_OPENDATASERVER_H
