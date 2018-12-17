7d0ff741c206080f1bc0eb647e09788f7383fb8b
oded282 <oded.bennoonshamir@live.biu.ac.il>
2018-12-17 20:33:52 +0200

    17.12 1644

--- main.cpp
+++ main.cpp
@@ -8,16 +8,11 @@
 using namespace std;
 
 
-void  cleanWhiteSpaces(string& sentence){
-    sentence.erase( remove( sentence.begin(), sentence.end(), ' ' ), sentence.end() );
-}
-
 
 
 int main(int argc, char* argv[]) {
 
-    string s = "5*3 + 2 ,-5";
-    cleanWhiteSpaces(s);
+
 
 
     return 0;
