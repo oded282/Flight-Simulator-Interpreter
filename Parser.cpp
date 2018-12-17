62835d33cad855315a1dacacb00c0d255766263a
almog931 <almog.levi@live.biu.ac.il>
2018-12-17 22:59:13 +0200

    17/12/2018 23:00

---
+++ Parser.cpp
@@ -0,0 +1,23 @@
+#include "Parser.h"
+
+Parser::Parser(const vector<string> &parser, int index) {
+
+    Parser::parser = parser;
+    Parser::index = index;
+
+}
+
+const vector<string> &Parser::getParser() const {
+    return parser;
+}
+
+int Parser::getIndex() const {
+    return index;
+}
+
+void Parser::setParser(const vector<string> &parser) {
+    Parser::parser = parser;
+}
+
+
+
